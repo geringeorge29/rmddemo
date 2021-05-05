324e9e88c67c71acb99486925c49573baa724c53
Gerin George <gering92@uw.edu>
2021-05-04 17:37:37 -0700

    RMD Demo files

---
+++ analysis.R
@@ -0,0 +1,28 @@
+library(pscl)
+library(dplyr)
+
+hofstede <- read.csv(
+  "https://geerthofstede.com/wp-content/uploads/2016/08/6-dimensions-for-website-2015-08-16.csv",
+  stringsAsFactors = FALSE,
+  sep = ";"
+)
+
+# With chaining
+idv_data_df <- hofstede %>% 
+  select(country,idv) %>% 
+  mutate(idv = as.numeric(idv)) %>% 
+  filter(!is.na(idv)) %>% 
+  arrange(idv)
+
+idv_summary_chain <- idv_data_df %>% 
+  summarize(avg_idv = mean(idv),
+            min_idv = min(idv),
+            max_idv = max(idv))
+
+max_idv_country_chain <- idv_data_df %>% 
+  filter(idv == max(idv)) %>% 
+  pull(country)
+
+min_idv_country_chain <- idv_data_df %>% 
+  filter(idv == min(idv)) %>% 
+  pull(country)
